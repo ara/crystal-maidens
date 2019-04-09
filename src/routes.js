2fa1a27e0b6a610f2979333bd8cc6a8743d4e3c4
Ara <aranarth78@gmail.com>
2019-04-09 18:34:38 +0200

    added '/items' route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue'
 import Maps from './components/Maps.vue'
 import Heroes from './components/Heroes.vue'
+import Items from './components/Items.vue'
 
 export const routes = [
   { path: '', component: Home },
   { path: '/maps', component: Maps },
   { path: '/maidens', component: Heroes },
+  { path: '/items', component: Items },
 ];
