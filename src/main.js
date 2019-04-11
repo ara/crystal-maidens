c56fc446ec9d87d3b48aaef8798131f473fd9361
Ara <aranarth78@gmail.com>
2019-04-11 11:21:27 +0200

    removed external tooltip component

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './routes';
 import './registerServiceWorker'
-import tooltipDirective from './directives/tooltip-directive';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
-Vue.use(tooltipDirective);
 
 const router = new VueRouter({
   routes,
