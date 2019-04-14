33df50f8b160be37f7b0cc2785f93092e2676a4f
Ara <aranarth78@gmail.com>
2019-04-14 23:08:48 +0200

    Initial commit

---
+++ src/components/GearSlot.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="container"
+    @click="onSlotClicked($event)"
+    @mouseenter="showTT($event, maiden)"
+    @mouseleave="hideTT($event)"
+  >
+    <img :src="uiImages.get('empty_slot_'+slot)" alt="itemSlots[slot]">
+  </div>
+</template>
+
+<script>
+import { itemSlots, SLOT, RARITY, CLASS, uiImages } from "../api/const.js";
+
+export default {
+  props: {
+    slot: Number,
+    maiden: Object
+  },
+
+  methods: {
+    onSlotClicked(e) {},
+    showTT(e, maiden) {},
+    hideTT(e) {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  cursor: pointer;
+  background-image: url("../assets/ui/bg_item_slot.png");
+}
+</style>
