465bdcb7d1a75994a6c7cbe241e06ba6d015d3a6
Ara <aranarth78@gmail.com>
2019-04-19 09:33:28 +0200

    fleshed out

--- src/components/GearSlot.vue
+++ src/components/GearSlot.vue
@@ -1,34 +1,150 @@
 <template>
   <div
-    class="container"
+    v-on-clickaway="away"
+    class="container _gslot"
     @click="onSlotClicked($event)"
-    @mouseenter="showTT($event, maiden)"
-    @mouseleave="hideTT($event)"
   >
-    <img :src="uiImages.get('empty_slot_'+slot)" alt="itemSlots[slot]">
+    <img class="item-background" :src="itemBackgroundUrl">
+    <img
+      class="item-icon"
+      :src="itemIcon"
+    >
+    <div v-if="listLoaded" class="item-list" :id="itemListID">
+      <div v-show="displayList">
+      <gear-slot-item-list
+        :fn-hide="onILHide"
+        :itemSlot="itemSlot"
+        :maiden="maiden"
+      ></gear-slot-item-list>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { itemSlots, SLOT, RARITY, CLASS, uiImages } from "../api/const.js";
+import { mapMutations } from 'vuex';
+import { SLOT, RARITY, CLASS, uiImages, bgItems } from '../api/const.js';
+import { maxHeaderSize } from 'http';
+import { directive as onClickaway } from '../directives/vue-clickaway.js';
 
 export default {
+  directives: {
+    onClickaway: onClickaway,
+  },
+
+  components: {
+    'gear-slot-item-list': () => import('./GearSlotItemList'),
+  },
+
   props: {
-    slot: Number,
-    maiden: Object
+    itemSlot: {
+      type: Number,
+      required: true,
+    },
+    maiden: {
+      type: Object,
+      required: true,
+    },
+    item: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  data: () => {
+    return {
+      listLoaded: false,
+      displayList: false,
+    }
+  },
+
+  computed: {
+    itemListID () { return `m${this.maiden.id}s${this.itemSlot}` },
+    itemBackgroundUrl () {
+      console.log(this.item && this.item.rarity);
+      return this.item
+        ? bgItems.get(this.item.rarity)
+        : uiImages.get('bg_item_slot')
+    },
+    itemIcon() {
+      return this.item
+        ? this.item.imageUrl
+        : uiImages.get('empty_slot_' + this.itemSlot)
+    }
   },
 
   methods: {
-    onSlotClicked(e) {},
-    showTT(e, maiden) {},
-    hideTT(e) {}
-  }
+    onILHide () {
+      console.log('on item list hide');
+      this.displayList = false;
+    },
+    ...mapMutations(['setActiveItemList']),
+    away: function(e) {
+      // console.log('clicked away');
+      this.displayList = false;
+    },
+    onSlotClicked(e) {
+      console.log('slot clicked');
+      e.preventDefault();
+      e.stopPropagation();
+      // const popup = document.getElementById(this.itemListID);
+      this.listLoaded = true;
+      this.displayList = !this.displayList;
+      if( this.displayList ) {
+        // console.log(this.$vnode);
+        // this.setActiveItemList( this );
+      }
+      // this.displayList = true;
+      // popup.classList.remove('hide-item-list');
+      // this.$nextTick().then( () => {
+      // document.addEventListener('click', this.documentOnClick );
+      // });
+    },
+  },
+
 };
 </script>
 
 <style lang="scss" scoped>
+.hide-item-list {
+  display: none;
+}
 .container {
+  position: relative;
   cursor: pointer;
-  background-image: url("../assets/ui/bg_item_slot.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 39px;
+  height: 39px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: .05em;
+  &:hover > .item-background,
+  &:hover > .item-icon {
+    transform: scale(1.1);
+  }
+}
+.item-list {
+  position: absolute;
+  left: 38px;
+  top: 0;
+}
+
+.item-background {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.empty-icon,
+.item-icon {
+  position: absolute;
+  align-self: center;
+}
+
+.item-icon {
+  width: 75%;
+  height: 75%;
 }
 </style>
