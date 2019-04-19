98a2312f2947a5655ab218fa55cc068e83f9c08e
Ara <aranarth78@gmail.com>
2019-04-19 09:39:10 +0200

    style: moved from global

--- src/components/HeroSkill.vue
+++ src/components/HeroSkill.vue
@@ -354,4 +354,17 @@ li {
     transition: opacity .4s ease;
   }
 }
+
+
+.slide-fade-enter-active
+{
+  transition: all .3s ease;
+}
+// .slide-fade-leave-active {
+//   transition: opacity .3s ease;
+// }
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(-10px);
+  opacity: 0;
+}
 </style>
