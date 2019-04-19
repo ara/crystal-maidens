cfc4d6f41cacdc11abe43fee77760e501cf62621
Ara <aranarth78@gmail.com>
2019-04-19 09:33:09 +0200

    fleshed out

--- src/components/Gear.vue
+++ src/components/Gear.vue
@@ -1,21 +1,50 @@
 <template>
   <div class="wrapper">
+
+    <app-gear-slot
+      v-for="slot in 6" :key="slot"
+      :maiden="maiden" :itemSlot="slot-1"
+      :item="getItem(slot-1)"
+      v-item-tooltip="{ item: getItem(slot-1) }"
+    ></app-gear-slot>
   </div>
 </template>
 
 <script>
-import GearSlot from "./GearSlot";
-import { mapState } from "vuex";
-import { SLOT } from "../api/const.js";
+import GearSlot from './GearSlot';
+import { mapMutations } from 'vuex';
 
 export default {
   props: {
     maiden: Object
   },
 
+  data: () => ({
+    // maidenGear: null,
+  }),
+
+  computed: {
+    maidenGear () {
+      return this.$store.state.maidensGear[this.maiden.id];
+    }
+  },
+
+  methods: {
+    ...mapMutations(['initMaidenGear']),
+    getItem (slot) {
+      return this.maidenGear ? this.maidenGear[slot] : null;
+    },
+  },
+
   components: {
-    "app-gear-slot": GearSlot
-  }
+    'app-gear-slot': GearSlot
+  },
+
+  created () {
+    if( !this.$store.state.maidensGear[this.maiden.id] ) {
+      this.initMaidenGear(this.maiden.id);
+    }
+  },
 };
 </script>
 
