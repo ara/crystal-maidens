ac781a754a6011fba316a8d24c02ac4d98ec3e40
Ara <aranarth78@gmail.com>
2019-04-23 23:36:43 +0200

    proto: global load/save of whole vuex state

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import heroes from './modules/heroes';
 import maps from './modules/maps';
 import items from './modules/items';
+import { throws } from 'assert';
 
 Vue.use(Vuex);
 
@@ -28,22 +29,30 @@ export default new Vuex.Store({
     },
 
     loadStore(state) {
-      if(localStorage.getItem('heroFilters')) {
-        Object.assign( state.heroes.filters, JSON.parse(localStorage.getItem('heroFilters')) );
-        this.replaceState( state )
+      const savedState = localStorage.getItem('state');
+      if( savedState ) {
+        Object.assign( state, JSON.parse( savedState ) );
+        this.replaceState(state);
       }
+      // if(localStorage.getItem('heroFilters')) {
+      //   Object.assign( state.heroes.filters, JSON.parse(localStorage.getItem('heroFilters')) );
+      //   this.replaceState( state )
+      //   Object.assign( state, JSON.parse(localStorage.getItem('state')) );
+      //   this.replaceState(state);
+      // }
     },
     watchStore(state) {
       // Subscribe to store updates
       this.subscribe((mutation, state) => {
-        switch( mutation.type ) {
-          case 'updateFilterHeroClass':
-          case 'updateFilterHeroElement':
-            localStorage.setItem('heroFilters', JSON.stringify(state.heroes.filters));
-            break;
-        }
+        localStorage.setItem('state', JSON.stringify(state));
+        // switch( mutation.type ) {
+        //   case 'updateFilterHeroClass':
+        //   case 'updateFilterHeroElement':
+        //     localStorage.setItem('heroFilters', JSON.stringify(state.heroes.filters));
+        //     break;
       });
-    }
+    },
+
   },
 
   strict: process.env.NODE_ENV !== 'production',
